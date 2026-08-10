52db07a9b64a5ed533263e11458c379145acc01d
SIDHARTH JAIN <jisidharthjain@gmil.com>
2026-08-11 01:45:02 +0530

    General Update

--- multi_doc_chat/utils/model_loader.py
+++ multi_doc_chat/utils/model_loader.py
@@ -89,7 +89,7 @@ class ModelLoader:
         """
 
         llm_block = self.config["llm"]
-        provider_key = os.getenv("LLM_PROVIDER" , "google")
+        provider_key = os.getenv("LLM_PROVIDER" , "groq")
 
         if provider_key not in llm_block : 
             log.error("LLM provider not found in config", provider=provider_key)
